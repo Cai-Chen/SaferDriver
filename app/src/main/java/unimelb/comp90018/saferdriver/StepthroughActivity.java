399c06ac133c5f0238b1abf01f8e540801565836
Rothschild888 <pengfei.xukafka@gmail.com>
2017-10-05 10:45:35 +1100

    format the code
    change the  comment

--- app/src/main/java/unimelb/comp90018/saferdriver/StepthroughActivity.java
+++ app/src/main/java/unimelb/comp90018/saferdriver/StepthroughActivity.java
@@ -16,15 +16,15 @@ import android.widget.ViewFlipper;
 public class StepthroughActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private ViewFlipper viewFlipper;
-    //定义手势检测器实例
+    //Defines an instance of the gesture detector
     GestureDetector detector;
-    //定义动画
+    //Custom Animation
     private Animation enter_left_to_right;
     private Animation exit_left_to_right;
 
     private Animation enter_right_to_left;
     private Animation exit_right_to_left;
-    //定义手势动作两点之间的最小距离
+    //Define the minimum distance between two points of gesture
     final int FLIP_DISTANCE=50;
 
     private ImageButton skip;
@@ -33,20 +33,20 @@ public class StepthroughActivity extends AppCompatActivity implements GestureDet
     protected void onCreate(Bundle savedInstanceState) {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //请求全屏
-        getWindow().requestFeature(Window.FEATURE_NO_TITLE); //请求没有标题栏
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //Request full screen
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE); //The request has no title bar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stepthrough);
 
         detector=new GestureDetector(this);
-        //获得ViewFlipper实例
+        //Get ViewFlipper instance
         viewFlipper = (ViewFlipper) this.findViewById(R.id.flipper);
-        /** 从左到右的两个动画 */
+        /** Two animations from left to right */
         enter_left_to_right = AnimationUtils.loadAnimation(this,
                 R.anim.enter_left_to_right);
         exit_left_to_right = AnimationUtils.loadAnimation(this,
                 R.anim.exit_left_to_right);
-        /** 从右到左的两个动画 */
+        /** Two animations from right to left */
         enter_right_to_left = AnimationUtils.loadAnimation(this,
                 R.anim.enter_right_to_left);
         exit_right_to_left = AnimationUtils.loadAnimation(this,
@@ -90,24 +90,18 @@ public class StepthroughActivity extends AppCompatActivity implements GestureDet
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // 将该Activity上的触碰时间交个 GestureDetector处理
+        // Hand in the touch time on the Activity, GestureDetector processor
         return detector.onTouchEvent(event);
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        /**
-         * 如果第1个触点事件的x坐标大于第2个触点事件的x坐标超过 FLIP_DISTANCE
-         * 也就是手势从右向左滑动
-         */
+
         if (e1.getX()-e2.getX()>FLIP_DISTANCE) {
             showPreviousView();
             return true;
         }
-        /**
-         * 如果第2个触点事件的x坐标大于第1个触点事件的x坐标超过 FLIP_DISTANCE
-         * 也就是手势从左向右滑动
-         */
+
         else if (e2.getX()-e1.getX()>FLIP_DISTANCE) {
             showNextView();
             return true;
@@ -115,26 +109,21 @@ public class StepthroughActivity extends AppCompatActivity implements GestureDet
         return false;
     }
 
-    /**
-     * 显示前一页
-     */
     private void showNextView() {
-        // 动画效果
+        // animation effect
         viewFlipper.setInAnimation(enter_left_to_right);
         viewFlipper.setOutAnimation(enter_left_to_right);
         viewFlipper.showPrevious();
     }
 
     /**
-     * 显示下一页
+     * Show the next page
      */
     private void showPreviousView() {
-        // 动画效果
+        // animation effect
         viewFlipper.setInAnimation(enter_right_to_left);
         viewFlipper.setOutAnimation(exit_right_to_left);
         viewFlipper.showNext();
     }
-
-
 
 }
